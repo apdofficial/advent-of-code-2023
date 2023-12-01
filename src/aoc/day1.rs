4dd0eb3dd978c9db1a58ff0eded00f66e31b9b3f
Andrej Pištek <andrej.pistek@outlook.com>
2023-12-01 16:02:31 +0100

    Day 1 solutions

--- src/aoc/day1.rs
+++ src/aoc/day1.rs
@@ -1,11 +1,70 @@
 use crate::aoc::common::PuzzleResult;
+use itertools::Itertools;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref NUMBERS_MAP: HashMap<&'static str, &'static str> = [
+        ("one", "o1e"),
+        ("two", "t2o"),
+        ("three", "t3e"),
+        ("four", "f4r"),
+        ("five", "f5e"),
+        ("six", "s6x"),
+        ("seven", "s7n"),
+        ("eight", "e8t"),
+        ("nine", "n9e"),
+    ]
+    .iter()
+    .copied()
+    .collect();
+}
 
 pub fn part1(input_data: &str) -> PuzzleResult {
-    PuzzleResult::Text("Not implemented".into())
+    input_data
+        .split('\n')
+        .filter_map(digits_to_number)
+        .collect_vec()
+        .iter()
+        .sum()
 }
 
 pub fn part2(input_data: &str) -> PuzzleResult {
-    PuzzleResult::Text("Not implemented".into())
+    input_data
+        .split('\n')
+        .map(text_to_text_digit)
+        .filter_map(|x| digits_to_number(x.as_ref()))
+        .collect_vec()
+        .iter()
+        .sum()
+}
+
+fn text_to_text_digit(line: &str) -> String {
+    let mut line: String = line.to_owned();
+    for (old, new) in NUMBERS_MAP.iter() {
+        line = line.replace(old, new);
+    }
+    line
+}
+
+fn digits_to_number(line: &str) -> Option<u32> {
+    let a = line
+        .chars()
+        .find(|x| x.is_numeric())
+        .and_then(|x| x.to_digit(10));
+    let b = line
+        .chars()
+        .rev()
+        .find(|x| x.is_numeric())
+        .and_then(|x| x.to_digit(10));
+    if a.is_none() {
+        return None;
+    }
+    return Some(
+        format!("{}{}", a.unwrap(), b.unwrap())
+            .parse::<u32>()
+            .unwrap(),
+    );
 }
 
 #[cfg(test)]
