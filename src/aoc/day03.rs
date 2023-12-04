14b5a03e9b7e7a162af84c65149b798490bb2acd
Andrej Pištek <andrej.pistek@outlook.com>
2023-12-04 17:22:46 +0100

    refactored day 03 to use structt

--- src/aoc/day03.rs
+++ src/aoc/day03.rs
@@ -1,71 +1,65 @@
-use std::ops::Range;
-use lazy_static::lazy_static;
+use std::ops::{AddAssign, Range};
+use itertools::Itertools;
 use regex::{Match, Regex};
 
-lazy_static! {
-    static ref NUMBER_RE: Regex = Regex::new(r"[0-9]+").unwrap();
-    static ref GEAR_RE: Regex = Regex::new(r"[*]").unwrap();
+struct Schematics {
+    lines: Vec<String>,
 }
 
-pub fn part1(input: &str) -> u32 {
-    let lines: Vec<_> = input.split('\n').collect();
-    lines.iter().fold((0u32, 0usize), |(mut sum, line_nr), line| {
-        NUMBER_RE.find_iter(line).for_each(|m| {
-            if line_nr > 0 && any_neighbour_contains_symbol_at(lines[line_nr - 1], &m) || // above
-                line_nr < lines.len() - 2 && any_neighbour_contains_symbol_at(lines[line_nr + 1], &m) || // below
-                m.start().checked_sub(1).is_some_and(|x| line.chars().nth(x).is_some_and(is_symbol)) || // left
-                line.chars().nth(m.end()).is_some_and(is_symbol) { // right
-                sum += m.as_str().parse::<u32>().unwrap_or(0);
-            }
+impl Schematics {
+    pub fn new(input: &str) -> Self {
+        let mut schematics = Schematics { lines: vec![] };
+        schematics.lines.extend(input.split('\n').map(|x| x.into()));
+        schematics
+    }
+
+    pub fn fold<F, T: AddAssign + Copy>(&self, re: &str, init: T,  mut f: F) -> T where F: FnMut(T, usize, &Match) -> T {
+        let mut acc = init;
+        let mut line_nr = 0;
+        let reg = Regex::new(re).unwrap();
+        self.lines.iter().for_each(|line| {
+            reg.find_iter(line).for_each(|m| acc = f(acc, line_nr, &m) );
+            line_nr += 1;
         });
-        (sum, line_nr + 1)
-    }).0
+        acc
+    }
+
+    pub fn find_neighbours(&self, re: &str, line_nr: usize, m: &Match) -> Vec<Match> {
+        let reg = Regex::new(re).unwrap();
+        let mut matches = reg.find_iter(self.lines[line_nr].as_str()).collect_vec();
+        if line_nr > 0 { matches.extend(reg.find_iter(self.lines[line_nr - 1].as_str())); }
+        if line_nr < self.lines.len() - 1 { matches.extend(reg.find_iter(self.lines[line_nr + 1].as_str())); }
+        matches.iter().filter(|x| self.all_neighbours(line_nr, x).any(|i| m.start() <= i && i <= m.end()-1)).copied().collect()
+    }
+
+    fn all_neighbours(&self, line_nr: usize, m: &Match) -> Range<usize> {
+        Range {
+            start: m.start().checked_sub(1).unwrap_or(0),
+            end: if m.end() < self.lines[line_nr].as_str().chars().count() - 2 { m.end() + 1 } else { m.range().end },
+        }
+    }
+}
+pub fn part1(input: &str) -> u32 {
+    let schematics = Schematics::new(input);
+    schematics.fold(r"[0-9]+", 0, |mut sum, line_nr, m| {
+        let matches = schematics.find_neighbours(r"[^0-9.]", line_nr, m);
+        if matches.len() > 0 {
+            sum += m.as_str().parse::<u32>().unwrap_or(0);
+        }
+        sum
+    })
 }
 
 pub fn part2(input: &str) -> u32 {
-    let lines: Vec<_> = input.split('\n').collect();
-    lines.iter().fold((0u32, 0usize), |(mut sum, line_nr), line| {
-        GEAR_RE.find_iter(line).for_each(|gear| {
-            let mut matches: Vec<Match> = vec![];
-            if line_nr > 0 {
-                NUMBER_RE.find_iter(lines[line_nr - 1])
-                    .filter(|x| contains_gear(lines[line_nr], &gear, x))
-                    .for_each(|x| matches.push(x));
-            }
-            NUMBER_RE.find_iter(lines[line_nr])
-                .filter(|x| contains_gear(lines[line_nr], &gear, x))
-                .for_each(|x| matches.push(x));
-            if line_nr < lines.len() - 1 {
-                NUMBER_RE.find_iter(lines[line_nr + 1])
-                    .filter(|x| contains_gear(lines[line_nr], &gear, x))
-                    .for_each(|x| matches.push(x));
-            }
-            if matches.len() == 2 {
-                sum += matches.get(0).unwrap().as_str().parse::<u32>().unwrap_or(0) *
-                    matches.get(1).unwrap().as_str().parse::<u32>().unwrap_or(0);
-            }
-        });
-        (sum, line_nr + 1)
-    }).0
-}
-
-fn contains_gear(line: &str, gear: &Match, candidate: &Match) -> bool {
-    neighbours(line, candidate).any(|i| line.chars().nth(i).is_some_and(is_gear) && gear.start() == i)
-}
-
-fn is_gear(c: char) -> bool { c == '*' }
-
-fn is_symbol(c: char) -> bool { !c.is_numeric() && c != '.' }
-
-fn any_neighbour_contains_symbol_at(line: &str, candidate: &Match) -> bool {
-    neighbours(line, candidate).any(|i| line.chars().nth(i).is_some_and(is_symbol))
-}
-
-fn neighbours(line: &str, m: &Match) -> Range<usize> {
-    Range {
-        start: m.start().checked_sub(1).unwrap_or(0),
-        end: if m.end() < line.chars().count() - 2 { m.end() + 1 } else { m.range().end },
-    }
+    let schematics = Schematics::new(input);
+    schematics.fold(r"[*]", 0, |mut sum, line_nr, m| {
+        let matches = schematics.find_neighbours(r"[0-9]+", line_nr, m);
+        if matches.len() == 2 {
+            sum += matches.get(0).unwrap().as_str().parse::<u32>().unwrap_or(0) *
+                matches.get(1).unwrap().as_str().parse::<u32>().unwrap_or(0);
+        }
+        sum
+    })
 }
 
 #[cfg(test)]
